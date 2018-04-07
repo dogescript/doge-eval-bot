f10f594b3f18e56c024a0a6dd4e3fb62eaa245fa
AnEmortalKid <janmonterrubio@gmail.com>
2018-04-06 23:34:31 -0500

    log storage type

--- bot.js
+++ bot.js
@@ -78,9 +78,11 @@ var bot_options = {
 // Use a mongo database if specified, otherwise store in a JSON file local to the app.
 // Mongo is automatically configured when deploying to Heroku
 if (process.env.MONGO_URI) {
+    console.log('Mongo Storage')
     var mongoStorage = require('botkit-storage-mongo')({mongoUri: process.env.MONGO_URI});
     bot_options.storage = mongoStorage;
 } else {
+    console.log('JSON Storage')
     bot_options.json_file_store = __dirname + '/.data/db/'; // store user data in a simple JSON format
 }
 
